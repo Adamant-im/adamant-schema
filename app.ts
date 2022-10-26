c816803e9424b38707f23441f8eeb3f2cb4328d6
bludnic <victorcpla@gmail.com>
2022-10-26 22:21:23 +0100

    build: add Prettier, Husky and LintStaged

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import swaggerUI from 'swagger-ui-express';
+import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 const app = express();
-const swaggerDocument = require('./dist/schema.json');
+const swaggerDocument = require("./dist/schema.json");
 
-app.use(express.static('dist'));
-app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use(express.static("dist"));
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App listening on http://localhost:${PORT}`)
+  console.log(`App listening on http://localhost:${PORT}`);
 });

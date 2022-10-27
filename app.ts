93ad5db2e963fe8a996e6fe33a89d92a61abbffd
bludnic <victorcpla@gmail.com>
2022-10-27 16:01:14 +0100

    build(Github Workflows): bundle & deploy actions

--- app.ts
+++ app.ts
@@ -2,9 +2,9 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 
 const app = express();
-const swaggerDocument = require("./dist/schema.json");
+const swaggerDocument = require("./docs/schema.json");
 
-app.use(express.static("dist"));
+app.use(express.static("docs"));
 app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;

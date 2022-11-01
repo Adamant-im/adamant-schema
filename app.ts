02d9bf1886c1886917a58da1c251998f7e3df3ab
bludnic <victorcpla@gmail.com>
2022-11-01 13:59:32 +0000

    build(Github Workflows): rename /docs to /dist

--- app.ts
+++ app.ts
@@ -2,9 +2,9 @@ import express from "express";
 import swaggerUI from "swagger-ui-express";
 
 const app = express();
-const swaggerDocument = require("./docs/schema.json");
+const swaggerDocument = require("./dist/schema.json");
 
-app.use(express.static("docs"));
+app.use(express.static("dist"));
 app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
